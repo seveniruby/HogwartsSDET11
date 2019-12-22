391880db7e56694d749686293bb641e61c9ef615
seveniruby <seveniruby@gmail.com>
2019-12-22 11:59:58 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# HogwartsSDET11
---
+++ demo.py
@@ -0,0 +1,14 @@
+print(2 + 2)
+print('C:\\some\\name')
+print(r'C:\some\name')
+
+print('Py' 'thon')
+
+x='abc'
+print(f'C:\\some\\name\\{x}')
+
+print("C:\\some\\name\\{y}\\{x}".format(y=x, x=123))
+
+
+squares = [1, 4, 9, 16, 25]
+squares
